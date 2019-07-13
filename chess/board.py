c54a77083117fe9e81f98366c5c93692ead469c9
SajadR2000 <49997181+SajadR2000@users.noreply.github.com>
2019-07-13 17:23:05 +0430

    latest edition of board
    latest edition of board

--- chess/board.py
+++ chess/board.py
@@ -72,6 +72,6 @@ sq62 = squares(6, 8, "wocc", "white")
 sq63 = squares(7, 8, "wocc", "black")
 sq64 = squares(8, 8, "wocc", "white")
 squareslist = [sq1, sq2, sq3, sq4, sq5, sq6, sq7, sq8, sq9, sq10, sq11, sq12, sq13, sq14, sq15, sq16, sq17
-, sq18, sq19, sq20, sq21, sq22, sq23, sq24, sq25, sq26, sq27, sq28, sq29, sq30, sq31, sq32, sq33, sq34
+, sq1, sq19, sq20, sq21, sq22, sq23, sq24, sq25, sq26, sq27, sq28, sq29, sq30, sq31, sq32, sq33, sq34
 , sq35, sq36, sq37, sq38, sq39, sq40, sq41, sq42, sq43, sq44, sq45, sq46, sq47, sq48, sq49, sq50, sq51
 , sq52, sq53, sq54, sq55, sq56, sq57, sq58, sq59, sq60, sq61, sq62, sq63, sq64]
