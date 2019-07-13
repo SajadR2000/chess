f5d37edce99f9ddc6f4ee123f156dcb05b3d85b7
SajadR2000 <49997181+SajadR2000@users.noreply.github.com>
2019-07-13 17:21:50 +0430

    latest edition of piece file
    latest edition of piece file

--- chess/piece.py
+++ chess/piece.py
@@ -1,80 +1,66 @@
 class piece:
-    def __init__(self,color, currentx, currenty, x = 0, y = 0):
+    def __init__(self,color, currentx, currenty, ptype, x = 0, y = 0):
         self.color = color
         self.currentx = currentx
         self.currenty = currenty
         self.x = x
         self.y = y
+        self.ptype = ptype
     def movement(self):
-        self.currentx += self.x
-        self.currenty += self.y
+        if self.ptype == "king" and ((abs(self.x) == abs(self.y)) or (self.x == 0 and abs(self.y) == 1) or (self.y == 0 and abs(self.x) == 1)):
+            self.currentx += self.x
+            self.currenty += self.y
+        elif self.ptype == "queen" and ((self.x == 0) or (self.y == 0) or (abs(self.x) == abs(self.y))):
+            self.currentx += self.x
+            self.currenty += self.y
+        elif self.ptype == "knight" and ((abs(self.x) == 1 and abs(self.y) == 2) or (abs(self.y) == 1 and abs(self.x) == 2)):
+            self.currentx += self.x
+            self.currenty += self.y
+        elif self.ptype == "rook" and (self.x == 0 or self.y == 0):
+            self.currentx += self.x
+            self.currenty += self.y
+        elif self.ptype == "bishop" and (abs(self.x) == abs(self.y)):
+            self.currentx += self.x
+            self.currenty += self.y
+        elif (self.ptype == "pawn") and (self.x == 0) and (((self.y == 1 and self.color == "black") or (self.y == -1 and self.color == "white")) or (abs(self.y) == 2 and ((self.currenty == 2 and self.color == "black") or (self.currenty == 7 and self.color == "white")))):
+            self.currentx += self.x
+            self.currenty += self.y
+        else:
+            return False
 
 
-class king(piece):
-    def movecheck(self):
-        if (abs(self.x) == abs(self.y)) or (self.x == 0 and abs(self.y) == 1) or (self.y == 0 and abs(self.x) == 1):
-            super().movement()
-
-
-class queen(piece):
-    def movecheck(self):
-        if (self.x == 0) or (self.y == 0) or (abs(self.x) == abs(self.y)):
-            super().movement()
-
-
-class knight(piece):
-    def movecheck(self):
-        if (abs(self.x) == 1 and abs(self.y) == 2) or (abs(self.y) == 1 and abs(self.x) == 2):
-            super().movement()
-
-
-class rook(piece):
-    def movecheck(self):
-        if self.x == 0 or self.y == 0:
-            super().movement()
-
-
-class bishop(piece):
-    def movecheck(self):
-        if abs(self.x) == abs(self.y):
-            super().movement()
-
-
-class pawn(piece):     
-    def movecheck(self):
-        if self.x == 0: 
-            super().movement()
-
-
-kingw =king("white", 5, 8 )
-queenw = queen("white", 4, 8)
-rookw1 = rook("white", 1, 8)
-rookw2 = rook("white", 8, 8)
-knightw1 = knight("white", 2, 8)
-knightw2 = knight("white", 7, 8)
-bishopw1 = bishop("white", 3, 8)
-bishopw2 = bishop("white", 6, 8)
-pawnw1 = pawn("white", 1, 7)
-pawnw2 = pawn("white", 2, 7)
-pawnw3 = pawn("white", 3, 7)
-pawnw4 = pawn("white", 4, 7)
-pawnw5 = pawn("white", 5, 7)
-pawnw6 = pawn("white", 6, 7)
-pawnw7 = pawn("white", 7, 7)
-pawnw8 = pawn("white", 8, 7)
-kingb = king("black", 5, 1 )
-queenb = queen("black", 4, 1)
-rookb1 = rook("black", 1, 1)
-rookb2 = rook("black", 8, 1)
-knightb1 = knight("black", 2, 1)
-knightb2 = knight("black", 7, 1)
-bishopb1 = bishop("black", 3, 1)
-bishopb2 = bishop("black", 6, 1)
-pawnb1 = pawn("black", 1, 2)
-pawnb2 = pawn("black", 2, 2)
-pawnb3 = pawn("black", 3, 2)
-pawnb4 = pawn("black", 4, 2)
-pawnb5 = pawn("black", 5, 2)
-pawnb6 = pawn("black", 6, 2)
-pawnb7 = pawn("black", 7, 2)
-pawnb8 = pawn("black", 8, 2)
+kingw =piece("white", 5, 8, "king" )
+queenw = piece("white", 4, 8, "queen")
+rookw1 = piece("white", 1, 8, "rook")
+rookw2 = piece("white", 8, 8, "rook")
+knightw1 = piece("white", 2, 8, "knight")
+knightw2 = piece("white", 7, 8, "knight")
+bishopw1 = piece("white", 3, 8, "bishop")
+bishopw2 = piece("white", 6, 8, "bishop")
+pawnw1 = piece("white", 1, 7, "pawn")
+pawnw2 = piece("white", 2, 7, "pawn")
+pawnw3 = piece("white", 3, 7, "pawn")
+pawnw4 = piece("white", 4, 7, "pawn")
+pawnw5 = piece("white", 5, 7, "pawn")
+pawnw6 = piece("white", 6, 7, "pawn")
+pawnw7 = piece("white", 7, 7, "pawn")
+pawnw8 = piece("white", 8, 7, "pawn")
+kingb = piece("black", 5, 1, "king")
+queenb = piece("black", 4, 1, "queen")
+rookb1 = piece("black", 1, 1, "rook")
+rookb2 = piece("black", 8, 1, "rook")
+knightb1 = piece("black", 2, 1, "knight")
+knightb2 = piece("black", 7, 1, "knight")
+bishopb1 = piece("black", 3, 1, "bishop")
+bishopb2 = piece("black", 6, 1, "bishop")
+pawnb1 = piece("black", 1, 2, "pawn")
+pawnb2 = piece("black", 2, 2, "pawn")
+pawnb3 = piece("black", 3, 2, "pawn")
+pawnb4 = piece("black", 4, 2, "pawn")
+pawnb5 = piece("black", 5, 2, "pawn")
+pawnb6 = piece("black", 6, 2, "pawn")
+pawnb7 = piece("black", 7, 2, "pawn")
+pawnb8 = piece("black", 8, 2, "pawn")
+pieceslist = [kingw, kingb, queenb, queenw, rookb1, rookb2, rookw1, rookw2, knightb1, knightb2, knightw1, knightw2
+, bishopb1, bishopb2, bishopw1, bishopw2, pawnb1, pawnb2, pawnb3, pawnb4, pawnb5, pawnb6, pawnb7, pawnb8, pawnw1, pawnw2
+, pawnw3, pawnw4, pawnw5, pawnw6, pawnw7, pawnw8]
