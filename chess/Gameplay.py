5034fcdf1045e6bba91218be740f2b724b7d0a6d
SajadR2000 <49997181+SajadR2000@users.noreply.github.com>
2019-07-16 20:54:14 +0430

    Update Gameplay.py

--- chess/Gameplay.py
+++ chess/Gameplay.py
@@ -1,5 +1,69 @@
-import piece2
+import piece
 import board
+def searchsquare(x, y):
+    for i in board.squareslist:
+        if i.x == x and i.y == y:
+            return i
+    return 0
+    
+
+def searchpiece(x, y):
+    for i in piece.pieceslist:
+        if i.currentx == x and i.currenty == y:
+            return i
+    return 0
+
+
+def validmove(piece, dx, dy):
+    cx = piece.currentx
+    cy = piece.currenty
+    fx = piece.currentx + dx
+    fy = piece.currenty + dy
+    if cx + dx > 8 or cx + dx <= 0 or cy + dy > 8 or cy + dy <= 0:
+        print("invalidmove")
+        return 0
+    if dx == dy and dx < 0:
+        cx -= 1
+        cy -= 1
+        while cx != fx and cy != fy:    
+            x = searchsquare(cx, cy)
+            if x.state != "empty":
+                print("invalidmove")
+                return 0
+            cx -= 1
+            cy -= 1
+        return 1
+    elif dx == dy and dx > 0:
+        cx += 1
+        cy += 1
+        while cx != fx and cy != fy:    
+            x = searchsquare(cx, cy)
+            if x.state != "empty":
+                print("invalidmove")
+                return 0
+            cx += 1
+            cy += 1
+        return 1
+    elif dx == 0:
+        cy += dy/abs(dy)
+        while cy != fy:
+            x = searchsquare(cx, cy)
+            if x.state != "empty":
+                print("invalidmove")
+                return 0
+            cy += dy/abs(dy)
+        return 1
+    elif dy == 0:
+        cx += dx/abs(dx)
+        while cx != fx:
+            x= searchsquare(cx, cy)
+            if x.state != "empty":
+                print("invalidmove")
+                return 0
+            cx += dx/abs(dx)
+        return 1
+
+
 while True:
     curntx = int(input("enter current x: ")) #coordinates of chosen square. 2000 means quit
     if curntx == 2000:
@@ -9,33 +73,20 @@ while True:
     deltay = int(input("enter delta y: "))   #positive deltay equals going down and negetive deltay equals going up 
     i = 0
     emptysignal = 1 # 0 means bocc or wocc. 1 means empty
-    while i < len(piece2.pieceslist):
-        if curntx == piece2.pieceslist[i].currentx and curnty == piece2.pieceslist[i].currenty:
-            chosenpiece = piece2.pieceslist[i]
-            break
-        i += 1
-    i = 0
-    while i < len(board.squareslist):
-        if board.squareslist[i].x == curntx and board.squareslist[i].y == curnty:
-            chosensquare = board.squareslist[i]
-            break
-        i += 1
-    i = 0
+    chosenpiece = searchpiece(curntx, curnty)
+    chosensquare =  searchsquare(curntx, curnty)
+    if chosenpiece == 0 or chosensquare == 0:
+        print("Error!")
+        break
     chosenpiece.x = deltax
     chosenpiece.y = deltay
-    while i < len(board.squareslist):
-        if board.squareslist[i].x == (chosenpiece.currentx +chosenpiece.x) and board.squareslist[i].y == (chosenpiece.currenty +chosenpiece.y):
-            newchosensquare = board.squareslist[i]
-            break
-        i += 1
-    i = 0
-    while i < len(piece2.pieceslist):
-        if piece2.pieceslist[i].currentx == (chosenpiece.currentx +chosenpiece.x) and piece2.pieceslist[i].currenty == (chosenpiece.currenty +chosenpiece.y):
-            piecetobedeleted = piece2.pieceslist[i]
-            emptysignal = 0
-            break
-        i += 1
-    if chosenpiece.movement() != False:
+    newchosensquare = searchsquare(chosenpiece.currentx +chosenpiece.x, chosenpiece.currenty +chosenpiece.y)
+    piecetobedeleted = searchpiece(chosenpiece.currentx +chosenpiece.x, chosenpiece.currenty +chosenpiece.y)
+    if piecetobedeleted == 0:
+        del piecetobedeleted
+    else:
+        emptysignal = 0
+    if chosenpiece.movement() != False and validmove(chosenpiece, deltax, deltay) == 1:
         if emptysignal == 1:    
             chosensquare.state = "empty"
             if chosenpiece.color == "black":
@@ -49,14 +100,14 @@ while True:
                 chosenpiece.y = -1 * deltay
                 chosenpiece.movement()
             elif chosenpiece.color == "black" and newchosensquare.state == "wocc":
-                piece2.pieceslist.remove(piecetobedeleted)
+                piece.pieceslist.remove(piecetobedeleted)
                 newchosensquare.state = "bocc"
             elif chosenpiece.color =="white" and newchosensquare.state == "bocc":
-                piece2.pieceslist.remove(piecetobedeleted)
+                piece.pieceslist.remove(piecetobedeleted)
                 newchosensquare.state = "wocc"
     else:
         print("Error Enter again2!")
-for i in piece2.pieceslist:
+for i in piece.pieceslist:
     print(i.ptype)
 
                 
